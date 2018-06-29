b17d11312b938aadddc791a1bb0fb94a9c4569cc
PJ Valentini <pj.valentini@gmail.com>
2018-06-29 17:55:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
- return chocholateBars.unshift(array, element)
+
 }
-addElementToBeginningOfArray("Mounds", 1);
+
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   
